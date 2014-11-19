bfdcb5ed4169e7bcbd875d05fc0b7d0dc667d574
Sai Prajnan Emani <prajnan@Sais-MacBook-Pro-2.local>
2014-11-18 22:34:33 -0500

    Home Page added

---
+++ src/prajnan/hci/studygroupfinder/HomeFragment.java
@@ -0,0 +1,36 @@
+package prajnan.hci.studygroupfinder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import session.SessionManager;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HomeFragment extends Fragment {
+	
+	SessionManager session;
+	Map<String, String> userDetails = new HashMap<String,String>();
+	TextView welcomeText;
+	
+	public HomeFragment(){}
+	
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+			
+			 View rootView = inflater.inflate(R.layout.fragment_app_home, container, false);
+			 
+			 welcomeText = (TextView)rootView.findViewById(R.id.textView2);
+			 session = new SessionManager(getActivity());
+			 
+			 userDetails = session.getUserDetails();
+			 
+			 welcomeText.setText(welcomeText.getText().toString() + "" +userDetails.get("email"));
+			 
+			 return rootView;
+	 }
+
+}
